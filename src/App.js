d36edffc8aa7fbcc5789e06b48616e8a35117f46
khang phung <duykhangpdk0701@gmail.com>
2020-06-24 21:34:28 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import QuoteBox from "./quoteBox";
+
+function App() {
+  let backgroundColor;
+  return (
+    <div className="app">
+      <QuoteBox />
+    </div>
+  );
+}
+
+export default App;
